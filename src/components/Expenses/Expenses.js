06264653d46c76f44741bcc1c29df2a016ac86f2
abhishekagrahari123 <69042089+abhishekagrahari123@users.noreply.github.com>
2022-12-07 18:46:25 +0530

    handled no expense in a year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,27 +1,41 @@
-import './Expenses.css'
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card'
-import ExpensesFilter from './ExpenseFilter';
-import {useState} from 'react';
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
+import { useState } from "react";
 
-function Expenses(props){
-    
+function Expenses(props) {
+  const [newYear, setNewYear] = useState("2020");
 
-    const [newYear, setNewYear] = useState('2020');
+  function yearChangeHandler(yearChoosed) {
+    setNewYear(yearChoosed);
+  }
+  const filteredItems = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === newYear;
+  });
 
+  let outputList = <p>No expense found</p>;
+  if (filteredItems.length > 0)
+    outputList = filteredItems.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          date={expense.date}
+          amount={expense.amount}
+        />
+      );
+    });
 
-    function yearChangeHandler(yearChoosed){
-        setNewYear(yearChoosed);
-    }
-    const filteredItems = props.expenses.filter((expense) => { return expense.date.getFullYear().toString()===newYear});
-
-    return (
-            <Card className='expenses'>
-                <ExpensesFilter year={newYear} onYearChange={yearChangeHandler}></ExpensesFilter>
-                {filteredItems.map((expense) => { return <ExpenseItem key={expense.id} title={expense.title} date={expense.date} amount={expense.amount}/>})}
-            </Card>
-    )
-    
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        year={newYear}
+        onYearChange={yearChangeHandler}
+      ></ExpensesFilter>
+      {outputList}
+    </Card>
+  );
 }
 
 export default Expenses;
